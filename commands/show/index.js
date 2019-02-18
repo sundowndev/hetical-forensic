7ba78a558a7d3c56dfd35e65653b7e7bc8b8bcb3
sundowndev <raphael@crvx.fr>
2019-02-18 23:12:21 +0100

    Show command

--- commands/show/index.js
+++ commands/show/index.js
@@ -6,10 +6,10 @@ export const action = async ({ args, logger }) => {
 
     switch (data) {
         case 'students':
-            logger.table({ rows: global.data.students || [], name: 'Students' });
+            logger.table({ rows: global.data.students.slice(0, 50) || [], name: 'Students' });
             break;
         case 'enterprises':
-            logger.table({ rows: global.data.enterprises || [], name: 'Enterprises' });
+            logger.table({ rows: global.data.enterprises.slice(0, 50) || [], name: 'Enterprises' });
             break;
         default:
             logger.error('You need to specify a valid module');
