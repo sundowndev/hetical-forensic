7f2d0e1a1eb15802b01bcb80e9f3f82cda710c29
sundowndev <raphael@crvx.fr>
2019-03-18 12:01:23 +0100

    Linkedin investigation

--- scripts/linkedin_investigation.py
+++ scripts/linkedin_investigation.py
@@ -6,6 +6,6 @@ with open('../data/db.json') as f:
     data = json.load(f)
 
 for student in data['students']:
-    for url in search('site:linkedin.com intext:"HETIC" inurl:"/in/{}.{}"'.format(student['firstName'], student['lastName']), stop=1):
+    for url in search('site:linkedin.com intext:"HETIC" inurl:"/in/{}-{}"'.format(student['firstName'], student['lastName']), stop=1):
         print('Linkedin profile for {} {}: {}'.format(student['firstName'], student['lastName'], url))
         break
